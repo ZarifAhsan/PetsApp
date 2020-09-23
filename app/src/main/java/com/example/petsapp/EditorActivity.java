90ec33d919ed268073ea70c7a23dd66a3720412f
ZarifAhsan <mdzarifahsan@gmail.com>
2020-09-23 18:56:24 +0600

    data entry added

--- app/src/main/java/com/example/petsapp/EditorActivity.java
+++ app/src/main/java/com/example/petsapp/EditorActivity.java
@@ -13,6 +13,8 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
 
+import com.example.petsapp.data.PetContract;
+
 public class EditorActivity extends AppCompatActivity {
 
     /** EditText field to enter the pet's name */
@@ -31,7 +33,7 @@ public class EditorActivity extends AppCompatActivity {
      * Gender of the pet. The possible values are:
      * 0 for unknown gender, 1 for male, 2 for female.
      */
-    private int mGender = 0;
+    private int mGender = PetContract.PetsEntry.GENDER_UNKNOWN;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -69,11 +71,11 @@ public class EditorActivity extends AppCompatActivity {
                 String selection = (String) parent.getItemAtPosition(position);
                 if (!TextUtils.isEmpty(selection)) {
                     if (selection.equals(getString(R.string.gender_male))) {
-                        mGender = 1; // Male
+                        mGender = PetContract.PetsEntry.GENDER_MALE; // Male
                     } else if (selection.equals(getString(R.string.gender_female))) {
-                        mGender = 2; // Female
+                        mGender = PetContract.PetsEntry.GENDER_FEMALE; // Female
                     } else {
-                        mGender = 0; // Unknown
+                        mGender = PetContract.PetsEntry.GENDER_UNKNOWN; // Unknown
                     }
                 }
             }
@@ -81,7 +83,7 @@ public class EditorActivity extends AppCompatActivity {
             // Because AdapterView is an abstract class, onNothingSelected must be defined
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
-                mGender = 0; // Unknown
+                mGender = PetContract.PetsEntry.GENDER_UNKNOWN; // Unknown
             }
         });
     }
--- app/src/main/java/com/example/petsapp/data/PetContract.java
+++ app/src/main/java/com/example/petsapp/data/PetContract.java
@@ -1,4 +1,23 @@
 package com.example.petsapp.data;
 
-public class PetContract {
+import android.provider.BaseColumns;
+
+public final class PetContract {
+
+    private PetContract() {}
+
+
+    public static abstract class PetsEntry implements BaseColumns {
+
+        public static final String TABLE_NAME = "pets";
+        public static final String COLUMN_ID = BaseColumns._ID;
+        public static final String COLUMN_PET_NAME = "name";
+        public static final String COLUMN_PET_BREED = "breed";
+
+
+        public static final int GENDER_UNKNOWN = 0;
+        public static final int GENDER_MALE = 1;
+        public static final int GENDER_FEMALE = 2;
+
+    }
 }
