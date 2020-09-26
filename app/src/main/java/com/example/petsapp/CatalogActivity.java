78d1e824095cd06a435a54def219bacd9c02f648
ZarifAhsan <mdzarifahsan@gmail.com>
2020-09-26 20:57:14 +0600

    Dummy data demo functionality added

--- app/src/main/java/com/example/petsapp/CatalogActivity.java
+++ app/src/main/java/com/example/petsapp/CatalogActivity.java
@@ -2,10 +2,13 @@ package com.example.petsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -16,6 +19,10 @@ import com.example.petsapp.data.PetContract.PetEntry;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class CatalogActivity extends AppCompatActivity {
+
+    public static final String LOG_TAG = CatalogActivity.class.getName();
+
+    private PetDbHelper mDbHelper;
 
     public static final String SQL_CREATE_ENTRIES = "CREATE TABLE " + PetEntry.TABLE_NAME + " ("
             + PetEntry.COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
@@ -42,18 +49,19 @@ public class CatalogActivity extends AppCompatActivity {
             }
         });
 
-        displayDatabaseInfo();
+        mDbHelper = new PetDbHelper(this);
+         displayDatabaseInfo();
+
     }
 
-    /**
+    /**Z
      * Temporary helper method to display information in the onscreen TextView about the state of
      * the pets database.
      */
+    @SuppressLint("SetTextI18n")
     private void displayDatabaseInfo() {
         // To access our database, we instantiate our subclass of SQLiteOpenHelper
         // and pass the context, which is the current activity.
-        PetDbHelper mDbHelper = new PetDbHelper(this);
-
         // Create and/or open a database to read from it
         SQLiteDatabase db = mDbHelper.getReadableDatabase();
 
@@ -72,6 +80,20 @@ public class CatalogActivity extends AppCompatActivity {
         }
     }
 
+    private void insertPet() {
+        SQLiteDatabase db = mDbHelper.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(PetEntry.COLUMN_PET_NAME, "Toto");
+        values.put(PetEntry.COLUMN_PET_BREED, "Terrier");
+        values.put(PetEntry.COLUMN_PET_GENDER, PetEntry.GENDER_MALE);
+        values.put(PetEntry.COLUMN_PET_WEIGHT, 7);
+
+        long newRowId = db.insert(PetEntry.TABLE_NAME, null, values);
+
+        Log.e(LOG_TAG, "New Column: " + newRowId);
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu options from the res/menu/menu_catalog.xml file.
@@ -86,7 +108,9 @@ public class CatalogActivity extends AppCompatActivity {
         switch (item.getItemId()) {
             // Respond to a click on the "Insert dummy data" menu option
             case R.id.action_insert_dummy_data:
-                // Do nothing for now
+                insertPet();
+                displayDatabaseInfo();
+
                 return true;
             // Respond to a click on the "Delete all entries" menu option
             case R.id.action_delete_all_entries:
--- app/src/main/java/com/example/petsapp/data/PetDbHelper.java
+++ app/src/main/java/com/example/petsapp/data/PetDbHelper.java
@@ -9,7 +9,7 @@ import com.example.petsapp.CatalogActivity;
 public class PetDbHelper extends SQLiteOpenHelper {
 
     private static final int DATABASE_VERSION = 1;
-    private static final String DATABASE_NAME = "FeedReader.db";
+    private static final String DATABASE_NAME = "Shelter.db";
 
     public PetDbHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
